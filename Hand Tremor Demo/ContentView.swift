77cd3ad2bd2803fc56a401988abf5b61d34de12f
Abhishek Kurmi <itsabhi814@gmail.com>
2025-12-17 16:14:50 +0530

    Initial Commit

---
+++ Hand Tremor Demo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hand Tremor Demo
+//
+//  Created by Apple on 17/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hand Tremor Demo/Hand_Tremor_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hand_Tremor_DemoApp.swift
+//  Hand Tremor Demo
+//
+//  Created by Apple on 17/12/25.
+//
+
+import SwiftUI
+
+@main
+struct Hand_Tremor_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
